532077c586a91c46807571cf10ab5ab02edafb79
anlsim <anlsimental@gmail.com>
2021-10-16 21:04:36 -0700

    Edited login

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -9,23 +9,18 @@ export default function Login() {
   const passwordRef = useRef();
   const { dispatch, isFetching } = useContext(Context);
   const [error, setError] = useState(false);
-
   const handleLogin = async (e) => {
     e.preventDefault();
     setError(false);
     dispatch({ type: "LOGIN_START" });
-    console.log(emailRef.current.value)
-    console.log(emailRef.current.value.toLowerCase())
     try {
       const res = await axios.post("/api/auth/login", {
         email: emailRef.current.value.toLowerCase(),
         password: passwordRef.current.value,
       });
-      
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (err) {
       console.log(err)
-
       dispatch({ type: "LOGIN_FAILURE" });
       setError(true);
     }
@@ -50,7 +45,6 @@ export default function Login() {
                 <button className="mainForm-button" type="submit" disabled={isFetching}>Login</button>
             </form>
             <Link to="/resetPassword">Forgot Password?</Link>
-            
         </div>
     )
 }
