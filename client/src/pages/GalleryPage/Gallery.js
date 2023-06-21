3e7cab4bdfe179c831d3d8c1fdad0f10f9a8895b
anlsim <anlsimental@gmail.com>
2023-06-20 19:31:58 -0700

    updated gallery

--- client/src/pages/GalleryPage/Gallery.js
+++ client/src/pages/GalleryPage/Gallery.js
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Hero from '../../components/Hero/Hero'
-import GalleryGrid from 'react-grid-gallery'
+import { Gallery } from "react-grid-gallery";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 import './Gallery.scss'
 
-export default function Gallery() {
+export default function GalleryPage() {
   const [photos, setphotos] = useState([])
   const [loading, setloading] = useState(true)
+  const [index, setIndex] = useState(-1);
+
+  const handleClick = (index, item) => setIndex(index)
+
   useEffect(() => {
     const fetchPost = async () => {
       const res = await axios.get('/api/gallery')
@@ -15,27 +21,29 @@ export default function Gallery() {
     }
     fetchPost()
   }, [])
+
   if (loading) return <h4>Loading gallery...</h4>
+  const slides = photos.map(({ src}) => ({
+    src: src,
+  }))
+
   return (
     <>
       <Hero title={'Gallery'} />
       <div className='galleryContainer'>
-        {/* <GalleryGrid
+        <Gallery
           images={photos.reverse()}
           margin={10}
           rowHeight={200}
+          onClick={handleClick}
           enableImageSelection={false}
-        /> */}
-        {photos.map((photo, index) => (
-          <div
-            className='galleryContainer__thumbnail'
-            key={index}>
-            <img
-              className='galleryContainer__image'
-              src={photo.src}
-              alt={photo.caption}></img>
-          </div>
-        ))}
+        />
+        <Lightbox
+          slides={slides}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+      />
       </div>
     </>
   )
